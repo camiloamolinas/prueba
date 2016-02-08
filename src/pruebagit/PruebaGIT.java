2cd022a7c78946f3e49466b2970a784a379bb75c
Estudiante <Estudiante@168.176.25.209>
2016-02-08 15:56:11 -0500

    segundo commit de pruebaGIT

--- src/pruebagit/PruebaGIT.java
+++ src/pruebagit/PruebaGIT.java
@@ -15,7 +15,7 @@ public class PruebaGIT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hola Mundo");
+       System.out.println("Hola Mundo!!!");
         
     }
     
